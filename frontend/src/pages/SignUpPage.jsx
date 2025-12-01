1ed61534fad1d483e132870b25842bc4e9966234
tdbaophuc <tdbaophuc@gmail.com>
2025-12-01 15:02:01 +0700

    feat: add run command for deploy server

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { ShipWheelIcon } from "lucide-react";
 import { Link } from "react-router";
 import useSignUp from "../hooks/useSignup";
-
+import logoImage from "../assets/QPMess_Logo.png";
 
 
 const SignUpPage = () => {
@@ -22,17 +22,20 @@ const SignUpPage = () => {
   return (
     <div
       className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
-      data-theme="forest"
+      data-theme="corporate"
     >
       <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
         {/* SIGNUP FORM - LEFT SIDE */}
         <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
           {/* LOGO */}
           <div className="mb-4 flex items-center justify-start gap-2">
-            <ShipWheelIcon className="size-9 text-primary" />
-            <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
-              Streamify
-            </span>
+            <Link to="/" className="flex items-center justify-center">
+              <img 
+                src={logoImage} 
+                alt="QPMess_Logo" 
+                className="h-12 w-auto object-contain"
+              />
+            </Link>
           </div>
 
           {/* ERROR MESSAGE IF ANY */}
