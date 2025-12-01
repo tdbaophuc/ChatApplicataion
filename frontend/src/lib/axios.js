ffeeb32fad2f9d7bb9a6284ff5bcc32203a34193
tdbaophuc <tdbaophuc@gmail.com>
2025-12-01 16:26:37 +0700

    fix: add api_url for vercel

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5050/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  withCredentials: true, // Gửi cookie
 });
